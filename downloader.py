49d284a56f80d88db6363d730b31cb3b882983f7
UdithaIshan <uditha.ishan34@gmail.com>
2020-11-18 07:22:20 +0530

    search added

--- downloader.py
+++ downloader.py
@@ -1,3 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 driver = webdriver.Chrome()
 driver.get('https://google.com')
+searchBox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+searchBox.send_keys('Uditha Ishan' + Keys.ENTER)
