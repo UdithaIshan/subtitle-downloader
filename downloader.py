e4170d5336134e83ec5b662902feaadd6a0e6342
UdithaIshan <uditha.ishan34@gmail.com>
2020-11-18 10:28:38 +0530

    download works now

--- downloader.py
+++ downloader.py
@@ -4,18 +4,20 @@ from selenium.webdriver.common.keys import Keys
 import os
 import re
 
-fileList = os.listdir("E:\Mandalorian")
+fileList = os.listdir()
 regex = re.compile(".*\.(mp4|avi|flv|mkv|mov|wmv)$")
 
 filtered = list(filter(regex.match, fileList))
 print(filtered)
 
-serachTerm = 'https://www.podnapisi.net/en/subtitles/search/?keywords=' + filtered[0]
-print(serachTerm)
-
 driver = webdriver.Chrome()
-driver.get(serachTerm)
-downloadBtn = driver.find_element_by_xpath('//*[@id="page-body"]/div[4]/div/div[6]/table/tbody/tr[1]/td[1]/div[1]/a[1]')
-downloadBtn.click()
-# searchBox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-# searchBox.send_keys(serachTerm + Keys.ENTER)
+
+for item in filtered:
+    serachTerm = 'https://www.podnapisi.net/en/subtitles/search/?keywords=' + item
+    driver.get(serachTerm)
+    try:
+        downloadBtn = driver.find_element_by_xpath('//*[@id="page-body"]/div[4]/div/div[6]/table/tbody/tr[1]/td[1]/div[1]/a[1]')
+        downloadBtn.click()
+    except:
+        continue
+    
