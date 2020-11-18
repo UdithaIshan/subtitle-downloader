729d41093b6b7833089a0fbefcb596a7d57d0caf
UdithaIshan <uditha.ishan34@gmail.com>
2020-11-18 12:25:42 +0530

    Downloading to the same location now

--- downloader.py
+++ downloader.py
@@ -4,13 +4,30 @@ from selenium.webdriver.common.keys import Keys
 import os
 import re
 
+CHROMEDRIVER_PATH = "C:/Windows/chromedriver.exe"
 fileList = os.listdir()
 regex = re.compile(".*\.(mp4|avi|flv|mkv|mov|wmv)$")
+
+def newChromeBrowser(headless=True, downloadPath=None):
+    """ Helper function that creates a new Selenium browser """
+    options = webdriver.ChromeOptions()
+    if headless:
+        options.add_argument('headless')
+    if downloadPath is not None:
+        prefs = {}
+        os.makedirs(downloadPath, exist_ok=True)
+        prefs["profile.default_content_settings.popups"]=0
+        prefs["download.default_directory"]=downloadPath
+        options.add_experimental_option("prefs", prefs)
+        
+    browser = webdriver.Chrome(chrome_options=options, executable_path=CHROMEDRIVER_PATH)
+    return browser
+
 
 filtered = list(filter(regex.match, fileList))
 print(filtered)
 
-driver = webdriver.Chrome()
+driver = newChromeBrowser(downloadPath = os.getcwd())
 
 for item in filtered:
     serachTerm = 'https://www.podnapisi.net/en/subtitles/search/?keywords=' + item
