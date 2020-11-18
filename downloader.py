4973ee2edd3fe53bc3fa662b94dadc3b32b62cdd
UdithaIshan <uditha.ishan34@gmail.com>
2020-11-18 09:58:31 +0530

    downloading added

--- downloader.py
+++ downloader.py
@@ -4,12 +4,18 @@ from selenium.webdriver.common.keys import Keys
 import os
 import re
 
-fileList = os.listdir()
+fileList = os.listdir("E:\Mandalorian")
 regex = re.compile(".*\.(mp4|avi|flv|mkv|mov|wmv)$")
 
-filtered = filter(regex.match, fileList)
+filtered = list(filter(regex.match, fileList))
+print(filtered)
+
+serachTerm = 'https://www.podnapisi.net/en/subtitles/search/?keywords=' + filtered[0]
+print(serachTerm)
 
 driver = webdriver.Chrome()
-driver.get('https://google.com')
-searchBox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-searchBox.send_keys('Uditha Ishan' + Keys.ENTER)
+driver.get(serachTerm)
+downloadBtn = driver.find_element_by_xpath('//*[@id="page-body"]/div[4]/div/div[6]/table/tbody/tr[1]/td[1]/div[1]/a[1]')
+downloadBtn.click()
+# searchBox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+# searchBox.send_keys(serachTerm + Keys.ENTER)
